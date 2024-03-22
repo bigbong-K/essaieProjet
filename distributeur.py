f260566529dbc4be932461c5aa83eabefc185293
TATI <tatibongue@gmail.com>
2024-03-22 12:56:00 +0000

    Ajout de la methode __str__ et de la méthode prochain_jour

--- distributeur.py
+++ distributeur.py
@@ -2,3 +2,10 @@ class Boisson:
     def __init__(self, volume, peremption) -> None:
         self.volume = volume
         self.peremption = peremption
+
+    def __str__(self):
+        return f"Volume : {self.volume} ml\nPeremption : dans {self.peremption} jour(s)"
+
+    def prochain_jour(self):
+        self.peremption -= 1
+
